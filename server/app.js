1f2c2f311206896cc71a3a2ffa019fb37e27a6f7
kairadio <motivatedkairat@gmail.com>
2025-07-10 20:23:22 +0500

    Added mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
-const MONGO_URI = 'mongodb://localhost:27017/expense-tracker';
+const PORT = process.env.PORT || 3000;
+
+// MongoDB Atlas connection string
+const MONGO_URI = `mongodb+srv://intern:123abc@cluster0.pisis.mongodb.net/intern-expense-tracker?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.use(cors());
 app.use(express.json());
@@ -14,18 +17,12 @@ app.get('/', (req, res) => {
   res.send('Expense Tracker API is running.');
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
-});
-
 // Подключение к MongoDB и запуск сервера
-// mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
-//   });
-// }).catch((err) => {
-//   console.error('❌ Ошибка подключения к MongoDB:', err);
-// });
+mongoose.connect(MONGO_URI)
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
+  });
+}).catch((err) => {
+  console.error('❌ Ошибка подключения к MongoDB:', err);
+});
