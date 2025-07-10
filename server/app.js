b15c424ab39db6094f48926e3fe981f02989fda4
kairadio <motivatedkairat@gmail.com>
2025-07-10 20:10:52 +0500

    minor fix

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 const MONGO_URI = 'mongodb://localhost:27017/expense-tracker';
 
 app.use(cors());
@@ -14,14 +14,18 @@ app.get('/', (req, res) => {
   res.send('Expense Tracker API is running.');
 });
 
-// Подключение к MongoDB и запуск сервера
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
-  });
-}).catch((err) => {
-  console.error('❌ Ошибка подключения к MongoDB:', err);
+app.listen(PORT, () => {
+  console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
 });
+
+// Подключение к MongoDB и запуск сервера
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
+//   });
+// }).catch((err) => {
+//   console.error('❌ Ошибка подключения к MongoDB:', err);
+// });
